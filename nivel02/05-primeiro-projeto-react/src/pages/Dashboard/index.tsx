8f185275dab6292918c5642ba73ffed52528dbcd
herrmannluiza <herrmannluiza@gmail.com>
2020-09-23 16:36:57 -0300

    added repository item

--- nivel02/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ nivel02/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,21 @@ const Dashboard: React.FC = () => {
           <input placeholder="Digite o nome do repositório" />
           <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+          <a href="">
+            <img
+                src="https://avatars0.githubusercontent.com/u/17033335?s=460&u=8e9482ba6b3cf1db3cccd523726e02c08d16a1b5&v=4"
+                alt="Ana Herrmann"
+            />
+            <div>
+                <strong>anaherrmann/appdog</strong>
+                <p>App for animal adoption management</p>
+            </div>
+
+            <FiChevronRight size={20}/>
+          </a>
+      </Repositories>
     </>
   );
 };
